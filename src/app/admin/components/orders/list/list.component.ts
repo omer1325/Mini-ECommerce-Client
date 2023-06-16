7a05b315e53199cf866e69b1a2c4e46495fa6ce3
omer1325 <omer.ingec24@gmail.com>
2023-06-16 19:02:12 +0300

    Coding of Order Details

--- src/app/admin/components/orders/list/list.component.ts
+++ src/app/admin/components/orders/list/list.component.ts
@@ -4,7 +4,9 @@ import { MatTableDataSource } from '@angular/material/table';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { List_Order } from 'src/app/contracts/order/list_order';
+import { OrderDetailDialogComponent, OrderDetailDialogState } from 'src/app/dialogs/order-detail-dialog/order-detail-dialog.component';
 import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { DialogService } from 'src/app/services/common/dialog.service';
 import { OrderService } from 'src/app/services/common/models/order.service';
 
 @Component({
@@ -16,12 +18,13 @@ export class ListComponent extends BaseComponent{
   constructor(
     spinner: NgxSpinnerService, 
     private orderService: OrderService, 
-    private alertifyService: AlertifyService, 
+    private alertifyService: AlertifyService,
+    private dialogService: DialogService 
     ){
     super(spinner)
   }
 
-  displayedColumns: string[] = ['orderCode', 'userName', 'totalPrice', 'createdDate', 'delete'];
+  displayedColumns: string[] = ['orderCode', 'userName', 'totalPrice', 'createdDate', 'viewdetail', 'delete'];
   dataSource : MatTableDataSource<List_Order> = null
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -45,5 +48,15 @@ export class ListComponent extends BaseComponent{
   }
   async ngOnInit() {
     await this.getOrders();
+  }
+
+  showDetail(id: string){
+    this.dialogService.openDialog({
+      componentType: OrderDetailDialogComponent,
+      data: id,
+      options: {
+        width: "750px"
+      }
+    });
   }
 }
--- src/app/dialogs/dialog.module.ts
+++ src/app/dialogs/dialog.module.ts
@@ -8,17 +8,21 @@ import { FileUploadModule } from '../services/common/file-upload/file-upload.mod
 import { MatCardModule } from '@angular/material/card';
 import { BasketItemRemoveDialogComponent } from './basket-item-remove-dialog/basket-item-remove-dialog.component';
 import { ShoppingCompleteDialogComponent } from './shopping-complete-dialog/shopping-complete-dialog.component';
+import { OrderDetailDialogComponent } from './order-detail-dialog/order-detail-dialog.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     DeleteDialogComponent,
     SelectProductImageDialogComponent,
     BasketItemRemoveDialogComponent,
-    ShoppingCompleteDialogComponent
+    ShoppingCompleteDialogComponent,
+    OrderDetailDialogComponent
   ],
   imports: [
     CommonModule,
-    MatDialogModule, MatButtonModule, MatCardModule,
+    MatDialogModule, MatButtonModule, MatCardModule, MatTableModule, MatToolbarModule,
     FileUploadModule
   ]
 })
