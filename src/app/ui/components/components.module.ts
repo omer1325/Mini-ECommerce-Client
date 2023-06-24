129f89036e7767af6ac4446e44b3d77c1252160e
omer1325 <omer.ingec24@gmail.com>
2023-06-24 17:15:53 +0300

    Updating User Password with Reset Password

--- src/app/ui/components/components.module.ts
+++ src/app/ui/components/components.module.ts
@@ -5,11 +5,12 @@ import { HomeModule } from './home/home.module';
 import { ProductsModule } from './products/products.module';
 import { RegisterModule } from './register/register.module';
 import { LoginModule } from './login/login.module';
-
-
+import { PasswordResetModule } from './password-reset/password-reset.module';
+import { UpdatePasswordModule } from './update-password/update-password.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+  ],
   imports: [
     CommonModule,
     BasketsModule,
@@ -17,6 +18,8 @@ import { LoginModule } from './login/login.module';
     ProductsModule,
     RegisterModule,
     // LoginModule,
+    PasswordResetModule,
+    UpdatePasswordModule
   ],
   exports:[
     BasketsModule
